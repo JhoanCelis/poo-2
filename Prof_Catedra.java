d9f11aecae932b7ba331c2f1a55582ff6c47ebfb
JhoanCelis <112718872+JhoanCelis@users.noreply.github.com>
2022-09-27 09:20:03 -0500

    Add files via upload

---
+++ Prof_Catedra.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratorio.pkg2;
+
+/**
+ *
+ * @author estudiante
+ */
+public class Prof_Catedra extends Profesor{
+     int numContrato,numHoras;
+     double valorHora;
+    String categoria;
+    public Prof_Catedra() {
+    }
+
+    public Prof_Catedra(int numContrato, int numHoras, double valorHora) {
+        this.numContrato = numContrato;
+        this.numHoras = numHoras;
+        this.valorHora = valorHora;
+    }
+
+    public Prof_Catedra(int numContrato, int numHoras, double valorHora, String codigo, String nombre, String titulo, Departamento departamento) {
+        super(codigo, nombre, titulo, departamento);
+        this.numContrato = numContrato;
+        this.numHoras = numHoras;
+        this.valorHora = valorHora;
+    }
+    
+    
+
+    
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+    public double calcularSalario(){
+        double salario=0;
+        if(categoria=="auxiliar"){
+            valorHora=20000;
+            salario=numHoras*valorHora;
+        }
+        if(categoria=="asistente"){
+            valorHora=30000;
+            salario=numHoras*valorHora;
+        }
+        if(categoria=="asistente"){
+            valorHora=30000;
+            salario=numHoras*valorHora;
+        }
+        return salario;
+    }
+}
